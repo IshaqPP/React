6177b6f5302e6308141dd94372e8c9bcc1d126bd
Ishaq <ishaqdop@gmail.com>
2023-09-19 12:01:42 +0530

    import and export

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import '../App.css';
 import List from './List';
 import Header from './Header';
 import Greetings from './Greetings'
+import {add,sub,mul,div} from './Calculate'
 
 function App() {
   
   return (
     <div>
       <Header />
-      <List />
-      <Greetings />
-
+      <ul>
+        <li>{add(20,10)}</li>
+        <li>{sub(1000,100)}</li>
+        <li>{mul(10,10)}</li>
+        <li>{div(100,10)}</li>
+      </ul>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return(
-        <h1>MY Favourite Football Players</h1>
+        <h1>Calculater</h1>
     );
 }
 
