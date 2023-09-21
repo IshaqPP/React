46ce617ad4a75ac887b6120dd2516361ed0f78ee
Ishaq <ishaqdop@gmail.com>
2023-09-21 12:56:20 +0530

    React Conditional Rendering with the Ternary Operator & AND Operator

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,12 @@
 import React from "react";
-import emojiList from "../emojiList";
-import EmojiCard from "./EmojiCard";
+import Form from "./Login";
 
-function emoji(SingleEmoji){
-  return <EmojiCard 
-    key={SingleEmoji.id}
-    emoji={SingleEmoji.emoji}
-    name={SingleEmoji.name}
-    meaning={SingleEmoji.meaning}
-  />
-}
-
-function cut(SingleEmoji) {
-  return SingleEmoji.meaning.substring(0,100);
-}
+let userIsRegistered = "false";
 
 function App() {
-  let str=emojiList.map(cut);
-  console.log(str);
   return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">
-      {emojiList.map(emoji)}
-      </dl>
+    <div className="container">
+      <Form IsRegistered = {userIsRegistered}/>
     </div>
   );
 }
