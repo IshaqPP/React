91a161e2e683fefa7ed0c7e4f1343476aa123546
Ishaq <ishaqdop@gmail.com>
2023-09-22 14:36:40 +0530

    Counter app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
-import Form from "./Login";
-
-let userIsRegistered = "false";
+import React,{useState} from "react";
 
 function App() {
+  let [count,setCount]=useState(0);
+
+  function increase() {
+    console.log("increase");
+    setCount(count+1);
+  }
+  
+  function decrease() {
+    console.log("decrease");
+    setCount(count-1);
+  
+  }
+  
   return (
     <div className="container">
-      <Form IsRegistered = {userIsRegistered}/>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
